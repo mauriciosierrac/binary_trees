98b24ce0fe4eb1ce6e8f62fd2d8981f6e97ee768
mauricio.sierrac <2376@holbertonschool.com>
2021-03-09 00:53:38 +0000

    create task0 :beers:

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a new node in the tree
+ * @parent: pointer to node parent
+ * @value: value field
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+	{
+		return NULL;
+	}
+
+	new->n = value;
+	new->parent = parent;
+	new->left = new->right = NULL;
+
+	return new;
+}
