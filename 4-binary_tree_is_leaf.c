4c693ece4a61bc453f06616263d19ccb641264eb
mauricio.sierrac <2376@holbertonschool.com>
2021-03-10 18:57:09 +0000

    task4 :beers:

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
+ * binary_tree_is_leaf - verify if is leaf
+ * @node: pointer to node
+ * Return: 1 if not a leaft or 0 if a leaft
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
