3d250068c43379acc9d064d70a7e000cfe842992
mauricio.sierrac <2376@holbertonschool.com>
2021-03-10 18:55:23 +0000

    task4 :beers:

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->left != NULL || node->right != NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
