df80ae60513fbdb97b0faf12fe1fe1f11e934ab8
mauricio.sierrac <2376@holbertonschool.com>
2021-03-11 20:10:14 +0000

    create task 17 y 18 :beers:

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find the sibling of a node
+ * @node: pointer to the node
+ * Return: NULL if node or parent i
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->right)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
+}
